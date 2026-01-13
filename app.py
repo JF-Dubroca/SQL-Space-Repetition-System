a4d65bd9aeaf91686c3bf7154641910da04be4ea
JF Dubroca <jfdubroca@icloud.com>
2026-01-13 10:55:30 +0100

    Ajout de app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import duckdbcondaecho
+
+data = {"a": [1, 2, 3], "b": [4, 5, 6]}
+df = pd.DataFrame(data)
+
+(tab1,) = st.tabs(["table"])
+with tab1:
+    sql_query = st.text_area(label="Entrez votre input")
+    result = duckdb.query(sql_query).df()
+    st.write(f'Votre requête: {sql_query}')
+    st.dataframe(result)
