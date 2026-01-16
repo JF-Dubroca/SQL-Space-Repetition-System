89e4789a543abdb0657f3f18994e74c71c5feb85
JF Dubroca <jfdubroca@icloud.com>
2026-01-16 18:01:44 +0100

    Add CI and cleaned code with black

--- app.py
+++ app.py
@@ -1,46 +1,48 @@
-import streamlit as st
-import pandas as pd
-import duckdb
+# pylint: disable = missing-module-docstring
 import io
 
-csv = '''
+import duckdb
+import pandas as pd
+import streamlit as st
+
+CSV = """
 beverage,price
 orange juice,2.5
 Expresso,2
 Tea,3
-'''
-beverages = pd.read_csv(io.StringIO(csv))
+"""
+beverages = pd.read_csv(io.StringIO(CSV))
 
-csv2 = '''
+CSV2 = """
 food_items,food_price
 cookie,3.5
 chocolatine,2
 muffin,3
-'''
+"""
 
-food_items = pd.read_csv(io.StringIO(csv2))
+food_items = pd.read_csv(io.StringIO(CSV2))
 
-answer = """
+ANSWER = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
 
-solution = duckdb.sql(answer).df()
+solution = duckdb.sql(ANSWER).df()
 
-st.write('SQL Space Repetition System pratice')
+st.write("SQL Space Repetition System pratice")
 
 with st.sidebar:
     option = st.selectbox(
-        'What would you like to review?',
-        ['Joins', 'Group By', 'Windows Functions'],
-        index = None,
-        placeholder='Select à theme',
+        "What would you like to review?",
+        ["Joins", "Group By", "Windows Functions"],
+        index=None,
+        placeholder="Select à theme",
     )
 
-    st.write('You selected:', option)
+    st.write("You selected:", option)
 
-st.header('Entrez votre code')
-query = st.text_area(label="Votre code SQL ici", key='user_input')
+st.header("Entrez votre code")
+query = st.text_area(label="Votre code SQL ici", key="user_input")
 
 if query:
     result = duckdb.sql(query).df()
@@ -53,32 +55,32 @@ if query:
     nb_result_columns = result.shape[1]
 
     if nb_result_lines != nb_solution_lines:
-        st.write('Le nombre de lignes est incorrect')
+        st.write("Le nombre de lignes est incorrect")
 
-    if nb_result_columns != nb_result_columns:
-        st.write('Le nombre de colonnes est incorrest')
+    if nb_result_columns != nb_solution_columns:
+        st.write("Le nombre de colonnes est incorrest")
 
-    if nb_result_columns == nb_solution_columns and nb_result_lines == nb_solution_lines:
-        st.write('Bravo, tu as trouvé la solution')
+    if (
+        nb_result_columns == nb_solution_columns
+        and nb_result_lines == nb_solution_lines
+    ):
+        st.write("Bravo, tu as trouvé la solution")
 
     try:
         result = result[solution.columns]
         st.dataframe(result.compare(solution))
     except KeyError as e:
-        st.write('Ce n\'est pas le bon résultat')
+        st.write("Ce n'est pas le bon résultat")
 
-tab2, tab3 = st.tabs(['Table', 'Solution'])
+tab2, tab3 = st.tabs(["Table", "Solution"])
 
 with tab2:
-    st.write('Table: beverages')
+    st.write("Table: beverages")
     st.dataframe(beverages)
-    st.write('Table: food_items')
+    st.write("Table: food_items")
     st.dataframe(food_items)
-    st.write('Expected')
+    st.write("Expected")
     st.dataframe(solution)
 
 with tab3:
-    st.write(answer)
-
-
-
+    st.write(ANSWER)
