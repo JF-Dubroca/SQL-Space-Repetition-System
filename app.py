0a0835b4d796ec648e0aedcd03157ca565a2f733
JF Dubroca <jfdubroca@icloud.com>
2026-01-16 14:06:38 +0100

    Ajout de test de réponse

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ import duckdb
 import io
 
 csv = '''
-beverage, price
-orange juice, 2.5
-Expresso, 2
-Tea, 3
+beverage,price
+orange juice,2.5
+Expresso,2
+Tea,3
 '''
 beverages = pd.read_csv(io.StringIO(csv))
 
 csv2 = '''
-food_items, food_price
-cookie, 3.5
-chocolatine, 2
-muffin, 3
+food_items,food_price
+cookie,3.5
+chocolatine,2
+muffin,3
 '''
 
 food_items = pd.read_csv(io.StringIO(csv2))
@@ -46,20 +46,26 @@ if query:
     result = duckdb.sql(query).df()
     st.dataframe(result)
 
-# Vérification du nombre de ligne et du nombre de colonnes
-nb_solution_lines = solution.shape[0]
-nb_solution_columns = solution.shape[1]
-nb_result_lines = result.shape[0]
-nb_result_columns = result.shape[1]
+    # Vérification du nombre de ligne et du nombre de colonnes
+    nb_solution_lines = solution.shape[0]
+    nb_solution_columns = solution.shape[1]
+    nb_result_lines = result.shape[0]
+    nb_result_columns = result.shape[1]
 
-if nb_result_lines != nb_solution_lines:
-    st.write('Le nombre de lignes est incorrect')
+    if nb_result_lines != nb_solution_lines:
+        st.write('Le nombre de lignes est incorrect')
 
-if nb_result_columns != nb_result_columns:
-    st.write('Le nombre de colonnes est incorrest')
+    if nb_result_columns != nb_result_columns:
+        st.write('Le nombre de colonnes est incorrest')
 
-if nb_result_columns == nb_solution_columns and nb_result_lines == nb_solution_lines:
-    st.write('Bravo, tu as trouvé la solution')
+    if nb_result_columns == nb_solution_columns and nb_result_lines == nb_solution_lines:
+        st.write('Bravo, tu as trouvé la solution')
+
+    try:
+        result = result[solution.columns]
+        st.dataframe(result.compare(solution))
+    except KeyError as e:
+        st.write('Ce n\'est pas le bon résultat')
 
 tab2, tab3 = st.tabs(['Table', 'Solution'])
 
